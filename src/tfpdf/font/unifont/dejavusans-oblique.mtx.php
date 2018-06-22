97c7440a93edab0430c8aede7d996b1bc285b67a
Rodrigo Queipo <rodrigoq@gmail.com>
2018-06-22 16:26:37 -0300

    Agrego tfpdf a framework (no se puede por composer).

---
+++ src/tfpdf/font/unifont/dejavusans-oblique.mtx.php
@@ -0,0 +1,18 @@
+<?php
+$name='DejaVuSans-Oblique';
+$type='TTF';
+$desc=array (
+	'Ascent' => 928,
+	'Descent' => -236,
+	'CapHeight' => 928,
+	'Flags' => 68,
+	'FontBBox' => '[-1016 -350 1659 1068]',
+	'ItalicAngle' => -11,
+	'StemV' => 87,
+	'MissingWidth' => 600,
+);
+$up=-63;
+$ut=44;
+$ttffile='/var/www/code/aacademica/src/lib/tfpdf/font/unifont/DejaVuSans-Oblique.ttf';
+$originalsize=611556;
+$fontkey='dejavuI';
