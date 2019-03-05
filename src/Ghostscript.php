0b82474b68f7c1c29a289b84d020836d9065ffed
Rodrigo Queipo <rodrigoq@gmail.com>
2019-03-05 17:36:59 -0300

    Bugfix en metadata de pdfs muy larga.

--- src/Ghostscript.php
+++ src/Ghostscript.php
@@ -11,22 +11,43 @@ class Ghostscript
 	 */
 	const GHOSTSCRIPT = "/gs";
 
+	// dejo un margen por caracteres unicode
+	// el máximo es en bytes pero los strings son multibyte.
+	const MetadataMaxLen = 400;
+
 	public static function Merge($coverFile, $originalFile, $targetFile, $title, $authors)
 	{
 		Profiling::BeginTimer();
 
-		$args = '-dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile='
-			. $targetFile . ' ' . $coverFile . ' ' . $originalFile . ' ';
+		self::TruncateMetadata($title, $authors);
 
-		$args .= " -c \"[/Producer(AAcademica.org)/Author "
-			. self::EscapeUnicode2($authors) . " /Title " . self::EscapeUnicode2($title) . " /DOCINFO pdfmark\"";
+		$args = '-dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile='
+			. $targetFile . ' ' . $coverFile . ' ' . $originalFile
+			. ' -c "[/Producer(AAcademica.org)/Author '
+			. self::EscapeUnicode2($authors) . ' /Title '
+			. self::EscapeUnicode2($title) . ' /DOCINFO pdfmark"';
 
 		IO::Delete($targetFile);
 
-		if (!self::RunGhostscript($args))
+		if (self::RunGhostscript($args) == false)
 			IO::Delete($targetFile);
 
 		Profiling::EndTimer();
+	}
+
+	public static function TruncateMetadata(&$title, &$authors)
+	{
+		$lenTitle = strlen($title);
+		if(strlen($authors) + $lenTitle > self::MetadataMaxLen)
+		{
+			if($lenTitle > self::MetadataMaxLen)
+			{
+				$authors = '';
+				$title = Str::Substr($title, 0, self::MetadataMaxLen);
+			}
+			else
+				$authors = Str::Substr($authors, 0, self::MetadataMaxLen - $lenTitle);
+		}
 	}
 
 	public static function EscapeUnicode2($cad)
@@ -43,7 +64,7 @@ class Ghostscript
 		$args = "-dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile="
 			. $targetFile . " -c .setpdfwrite -f " . $file;
 
-		if (!self::RunGhostscript($args))
+		if (self::RunGhostscript($args) == false)
 			IO::Delete($targetFile);
 
 		Profiling::EndTimer();
@@ -62,7 +83,7 @@ class Ghostscript
 
 		$args = "-dNOPAUSE -dBATCH -sDEVICE=jpeg -dFirstPage=1 -dAlignToPixels=0 -dGridFitTT=2 -dLastPage=1 -dTextAlphaBits=4 -dGraphicsAlphaBits=4 -dJPEGQ=100 -r30 -sOutputFile=" . $targetFile . " -c .setpdfwrite -f " . $file;
 
-		if (!self::RunGhostscript($args))
+		if (self::RunGhostscript($args) == false)
 			IO::Delete($targetFile);
 
 		Profiling::EndTimer();
