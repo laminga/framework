83f433b77ea2d2285530db842da13d58c29cf35a
Rodrigo Queipo <rodrigoq@gmail.com>
2019-03-08 15:34:05 -0300

    Bugix compilador, quito propiedad no declarada. Formato de variables.

--- src/ProfilingItem.php
+++ src/ProfilingItem.php
@@ -16,9 +16,9 @@ class ProfilingItem
 	public $hits;
 	public $dbHits;
 	public $isInternal;
-	private $start_pause;
+	private $startPause;
 
-	public $children = Array();
+	public $children = [];
 
 	public function __construct($name)
 	{
@@ -29,32 +29,32 @@ class ProfilingItem
 		$this->durationMs = 0;
 		$this->hits = 0;
 		$this->dbHits = 0;
-		$this->queries = [];
 		$this->memory = 0;
 		$this->memoryPeak = 0;
-		$this->start_pause = Performance::$pause_ellapsed_secs;
+		$this->startPause = Performance::$pauseEllapsedSecs;
 	}
 
 	private function ftime()
 	{
 		$a = gettimeofday();
-		return $a['sec'] + ($a['usec']*0.000001);
+		return $a['sec'] + ($a['usec'] * 0.000001);
 	}
 
 	public function CompleteTimer()
 	{
 		$t2 = microtime(true);
-		$end_pause = Performance::$pause_ellapsed_secs;
+		$endPause = Performance::$pauseEllapsedSecs;
 		$m2 = memory_get_usage();
 		/* $mp2 = memory_get_peak_usage(); */
 
 		$this->memory = $m2 - $this->startMemory;
 		/* $this->memoryPeak = $mp2 - $this->startMemoryPeak; */
 
-		$span = $t2 - $this->startTime - ($end_pause - $this->start_pause);
+		$span = $t2 - $this->startTime - ($endPause - $this->startPause);
 		$this->durationMs = $span * 1000;
 		$this->hits = 1;
 	}
+
 	public function SumChildren()
 	{
 		$this->memory = 0;
