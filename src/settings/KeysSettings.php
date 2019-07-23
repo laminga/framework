a23d1680a7bcb5b6d236285d265e11f999754ea9
Pablo De Grande <pablodg@gmail.com>
2019-07-23 11:07:27 -0300

    keys update

--- src/settings/KeysSettings.php
+++ src/settings/KeysSettings.php
@@ -6,27 +6,30 @@ use minga\framework\ErrorException;
 
 class KeysSettings
 {
-	//No usar directamente,
-	//usar los métodos GetXxxKey()
-	public $rememberKey = '';
-	public $hashKeyedKey = '';
+	public $RememberKey = '';
+	public $HashKeyedKey = '';
+	public $RecaptchaKey = '';
+	public $ExportNavigationKey = '';
 
-	public $recaptchaKey = '';
-	public $exportNavigationKey = '';
+	public $GoogleAnalyticsKey = '';
+	public $GoogleMapsKey = '';
+	public $AddThisKey = '';
+
+	public $SendGridApiKey = "";
 
 	public function GetHashKeyedKey()
 	{
-		if($this->hashKeyedKey == '')
+		if($this->HashKeyedKey == '')
 			throw new ErrorException('HashKeyed key not set. Please, add it to /config/settings.php file.');
 
-		return base64_decode($this->hashKeyedKey);
+		return base64_decode($this->HashKeyedKey);
 	}
 
 	public function GetRememberKey()
 	{
-		if($this->rememberKey == '')
+		if($this->RememberKey == '')
 			throw new ErrorException('Remember key not set. Please, add it to /config/settings.php file.');
 
-		return bin2hex(base64_decode($this->rememberKey));
+		return bin2hex(base64_decode($this->RememberKey));
 	}
 }
