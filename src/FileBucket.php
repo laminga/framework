eb34bb7e29b021f36615bf1016e3728588b2ebcf
WHY FUCK <cecilcrk@gmail.com>
2019-08-23 14:36:37 -0300

    Bugfix.

--- src/FileBucket.php
+++ src/FileBucket.php
@@ -73,7 +73,7 @@ class FileBucket
 
 	private function ResolvePath($id)
 	{
-		if ($id === null || trim($id) === '' || ctype_alnum($id) === false || sizeof($id) > 40)
+		if ($id === null || trim($id) === '' || ctype_alnum($id) === false || Str::Length($id) > 40)
 		{	// verifica este parámetro para evitar saltos en el filesystem fuera de tmp
 			throw new ErrorException('Invalid bucket Id');
 		}
