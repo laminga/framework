8d31013eebd08e6a4a282b617b25a40de065ab2f
Rodrigo Queipo <rodrigoq@gmail.com>
2020-04-14 19:14:17 -0300

    Fix en session.

--- src/PhpSession.php
+++ src/PhpSession.php
@@ -35,7 +35,8 @@ class PhpSession
 
 	private static function CheckPhpSessionStarted()
 	{
-		if (session_status() === PHP_SESSION_NONE)
+		if (isset($_SESSION) == false
+			&& session_status() === PHP_SESSION_NONE)
 		{
 			session_start();
 			if (self::$sessionValues == null)
