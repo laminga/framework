9c3f03ba71e4651602e579a9c6057f4db1e80168
Rodrigo Queipo <rodrigoq@gmail.com>
2020-04-16 12:17:58 -0300

    Mejoras compilador y tests.

--- compile.bat
+++ compile.bat
@@ -1,3 +1,11 @@
-call "C:\Program Files\Php Compilers\php\vendor\bin\phpstan.bat" analyse -c phpstan.neon -l 7 src\ --memory-limit 1024M>compile.txt
+@ECHO OFF
+IF %ComputerName% EQU CANTOR SET rq=1
+IF %ComputerName% EQU EULERUS SET rq=1
 
-start notepad compile.txt
+IF DEFINED rq (
+	CALL "C:\Users\Admin\Dropbox\Utils\php\vendor\bin\phpstan.bat" analyse -c phpstan.neon -l 5 src\ --memory-limit 1024M > compile.txt
+) ELSE (
+	CALL "C:\Program Files\Php Compilers\php\vendor\bin\phpstan.bat" analyse -c phpstan.neon -l 5 src\ --memory-limit 1024M > compile.txt
+	start notepad compile.txt
+)
+
