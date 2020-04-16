9479e7ae6e43d016c808bb8cda3cfc790a47bd53
Rodrigo Queipo <rodrigoq@gmail.com>
2020-04-16 16:04:17 -0300

    Scripts de pre-commit y pre-push e instalador.

--- compile.bat
+++ compile.bat
@@ -2,10 +2,11 @@
 IF %ComputerName% EQU CANTOR SET rq=1
 IF %ComputerName% EQU EULERUS SET rq=1
 
+CALL "vendor\bin\phpstan" analyse -c phpstan.neon -l 5 src\ --memory-limit 1024M > compile.txt
+
 IF DEFINED rq (
-	CALL "C:\Users\Admin\Dropbox\Utils\php\vendor\bin\phpstan.bat" analyse -c phpstan.neon -l 5 src\ --memory-limit 1024M > compile.txt
+	start compile.txt
 ) ELSE (
-	CALL "C:\Program Files\Php Compilers\php\vendor\bin\phpstan.bat" analyse -c phpstan.neon -l 5 src\ --memory-limit 1024M > compile.txt
 	start notepad compile.txt
 )
 
