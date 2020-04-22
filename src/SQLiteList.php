4a1afc0b5d32da323f593f449e15135f98ed7f94
Rodrigo Queipo <rodrigoq@gmail.com>
2020-04-22 11:37:06 -0300

    Formato.

--- src/SQLiteList.php
+++ src/SQLiteList.php
@@ -27,18 +27,23 @@ class SQLiteList
 		$this->commaArgs = "";
 		$n = 2;
 		if ($columns != null)
+		{
 			foreach($columns as $col)
 			{
 				$this->commaColumns .= "," . $col;
 				$this->commaArgs .= ",:p" . ($n++);
 			}
+		}
 		if ($intColumns != null)
+		{
 			foreach($intColumns as $col)
 			{
 				$this->commaColumns .= "," . $col;
 				$this->commaArgs .= ",:p" . ($n++);
 			}
+		}
 	}
+
 	public function Query($sql)
 	{
 		return $this->db->query($sql);
@@ -47,12 +52,12 @@ class SQLiteList
 	public function QueryAll($sql, $args = null)
 	{
 		if ($args != null && is_array($args) == false)
-			$args = array($args);
+			$args = [$args];
 		if ($args != null)
 			$result = $this->Execute($sql, $args);
 		else
 			$result = $this->Query($sql);
-		$ret = array();
+		$ret = [];
 		while($row = $result->fetchArray(SQLITE3_ASSOC))
 			$ret[] = $row;
 		return $ret;
@@ -63,8 +68,7 @@ class SQLiteList
 		$result = $this->Execute($sql, $params);
 		if ($result == null)
 			return null;
-		else
-			return $result->fetchArray(SQLITE3_ASSOC);
+		return $result->fetchArray(SQLITE3_ASSOC);
 	}
 
 	public function Open($path, $readonly = false)
@@ -75,9 +79,8 @@ class SQLiteList
 		$db = new \SQLite3($path, $flag);
 		$db->enableExceptions(true);
 		if ($existed == false)
-		{
 			$db->query(self::CreateSql());
-		}
+
 		$this->db = $db;
 		if ($readonly == false)
 			$this->db->busyTimeout(30000);
@@ -85,53 +88,59 @@ class SQLiteList
 		$this->Execute('PRAGMA synchronous=OFF');
 		$this->Execute('PRAGMA journal_mode=WAL');
 		Profiling::EndTimer();
-
 	}
 
 	private function CreateSql()
 	{
 		$sql = "CREATE TABLE data ("
-			. "pID     INTEGER PRIMARY KEY AUTOINCREMENT, "
-			. $this->keyColumn . " varchar(255) UNIQUE COLLATE NOCASE ";
+			. "pID INTEGER PRIMARY KEY AUTOINCREMENT, "
+			. $this->keyColumn . " VARCHAR(255) UNIQUE COLLATE NOCASE ";
 		if ($this->columns != null)
+		{
 			foreach($this->columns as $column)
 			{
-				$sql .=	", " . $column . " varchar(255) ";
+				$sql .=	", " . $column . " VARCHAR(255) ";
 				if ($this->uniqueColumns != null && in_array($column, $this->uniqueColumns))
 					$sql .= " UNIQUE";
 				$sql .= " COLLATE NOCASE ";
 			}
+		}
 		if ($this->intColumns != null)
+		{
 			foreach($this->intColumns as $column)
 				$sql .=	", " . $column . " integer ";
+		}
 
 		$sql .=	")";
 		return $sql;
 	}
+
 	public function Close()
 	{
 		Profiling::BeginTimer();
 		$this->db->close();
 		Profiling::EndTimer();
 	}
+
 	public function InsertOrUpdate()
 	{
 		$args = func_get_args();
-		if (sizeof($args) == 1 && is_array($args[0]))
+		if (count($args) == 1 && is_array($args[0]))
 			$args = $args[0];
 
-		$sql = "insert or replace into data (pID, " . $this->keyColumn . $this->commaColumns . ") values
-			((select pID from data where " . $this->keyColumn . " = :p1), :p1 " . $this->commaArgs . ");";
+		$sql = "INSERT OR REPLACE INTO data (pID, " . $this->keyColumn . $this->commaColumns . ") VALUES
+			((SELECT pID FROM data WHERE " . $this->keyColumn . " = :p1), :p1 " . $this->commaArgs . ");";
 
 		$this->Execute($sql, $args);
 	}
 
-	public function Execute($sql, $args = array())
+	public function Execute($sql, $args = [])
 	{
 		if (is_array($args) == false)
-			$args = array($args);
+			$args = [$args];
 		$text = $this->ParamsToText($sql, $args);
-		try {
+		try
+		{
 			$this->db->enableExceptions(true);
 			$statement = $this->db->prepare($sql);
 			$n = 1;
@@ -144,13 +153,13 @@ class SQLiteList
 			throw new ErrorException($text . '. Error nativo: ' . $e->getMessage() . ".");
 		}
 	}
+
 	private function ParamsToText($sql, $args)
 	{
 		$text = 'No se ha podido completar la operación en SQLite. ';
 		if ($this->path != null)
-		{
 			$text .= 'Path: ' . $this->path;
-		}
+
 		$text .= '. Comando: ' . $sql;
 		$paramsAsText = '';
 		foreach($args as $arg)
@@ -167,11 +176,11 @@ class SQLiteList
 	public function Insert()
 	{
 		$args = func_get_args();
-		if (sizeof($args) == 1 && is_array($args[0]))
+		if (count($args) == 1 && is_array($args[0]))
 			$args = $args[0];
 
 		// Arma el insert
-		$sql = "insert into data (" . $this->keyColumn . $this->commaColumns . ") values
+		$sql = "INSERT INTO data (" . $this->keyColumn . $this->commaColumns . ") VALUES
 			(:p1 " . $this->commaArgs . ");";
 
 		$statement = $this->db->prepare($sql);
@@ -183,7 +192,7 @@ class SQLiteList
 
 	public function Update($key, $column, $value)
 	{
-		$sql = "update data set " . $column . " = :p2 where " . $this->keyColumn . " = :p1;";
+		$sql = "UPDATE data SET " . $column . " = :p2 WHERE " . $this->keyColumn . " = :p1;";
 
 		$statement = $this->db->prepare($sql);
 		$statement->bindValue(':p1', $key);
@@ -194,46 +203,56 @@ class SQLiteList
 
 	public function DeleteAll()
 	{
-		$sql = "delete from data";
+		$sql = "DELETE FROM data";
 		$statement = $this->db->prepare($sql);
 		$statement->execute();
 	}
+
 	public function Delete($key)
 	{
-		$sql = "delete from data where " . $this->keyColumn . " = :p1;";
+		$sql = "DELETE FROM data WHERE " . $this->keyColumn . " = :p1;";
 
 		$statement = $this->db->prepare($sql);
 		$statement->bindValue(':p1', $key);
 
 		$statement->execute();
 	}
+
 	public function Truncate()
 	{
 		$this->Close();
 		unlink($this->path);
 	}
+
 	public function ReadValue($key, $column)
 	{
-	Profiling::BeginTimer();
-		$sql = "select pID, ". $column .
-			" from data where " . $this->keyColumn . " = :p1;";
+		try
+		{
+			Profiling::BeginTimer();
+			$sql = "SELECT pID, ". $column .
+				" FROM data WHERE " . $this->keyColumn . " = :p1;";
 
-		$statement = $this->db->prepare($sql);
-		$statement->bindValue(':p1', $key);
+			$statement = $this->db->prepare($sql);
+			$statement->bindValue(':p1', $key);
 
-		$result = $statement->execute();
+			$result = $statement->execute();
 
-		$res = $result->fetchArray(SQLITE3_NUM);
+			$res = $result->fetchArray(SQLITE3_NUM);
 
-		Profiling::EndTimer();
-		if ($res === false)
-			return null;
-		else
+			if ($res === false)
+				return null;
+
 			return $res;
+		}
+		finally
+		{
+			Profiling::EndTimer();
+		}
 	}
+
 	public function ReadRowByKey($key)
 	{
-		$sql = "select * from data where " . $this->keyColumn . " = :p1;";
+		$sql = "SELECT * FROM data WHERE " . $this->keyColumn . " = :p1;";
 
 		$statement = $this->db->prepare($sql);
 		$statement->bindValue(':p1', $key);
@@ -243,10 +262,8 @@ class SQLiteList
 		$res = $result->fetchArray(SQLITE3_ASSOC);
 
 		if ($res === false)
-		{
 			return null;
-		}else
-			return $res;
+		return $res;
 	}
 
 	public function Begin()
@@ -254,6 +271,7 @@ class SQLiteList
 		$this->Execute('PRAGMA journal_mode=DELETE');
 		$this->db->query("BEGIN TRANSACTION;");
 	}
+
 	public function Commit()
 	{
 		$this->db->query("COMMIT TRANSACTION;");
@@ -269,8 +287,8 @@ class SQLiteList
 			// update
 			$id = $res[0];
 			$n = intval($res[1]) + 1;
-			$sql = "update data set ". $column . " = " . $n .
-				" where pID = :p1;";
+			$sql = "UPDATE data SET ". $column . " = " . $n .
+				" WHERE pID = :p1;";
 			$statement = $this->db->prepare($sql);
 			$statement->bindValue(':p1', $id);
 
@@ -280,7 +298,7 @@ class SQLiteList
 		{
 			// insert
 			$n = 1;
-			$sql = "insert into data (" . $this->keyColumn . ", " . $column . ") values (:p1, :p2);";
+			$sql = "INSERT INTO data (" . $this->keyColumn . ", " . $column . ") VALUES (:p1, :p2);";
 			$statement = $this->db->prepare($sql);
 			$statement->bindValue(':p1', $key);
 			$statement->bindValue(':p2', $n);
