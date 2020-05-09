ed1a1489c0feba5887d9cbe0b7353f57abe47114
Rodrigo Queipo <rodrigoq@gmail.com>
2020-05-09 18:27:04 -0300

    Agrego Provider de datos global para tests para usar o no caches.

--- src/tests/TestCaseBase.php
+++ src/tests/TestCaseBase.php
@@ -4,20 +4,32 @@ namespace minga\framework\tests;
 
 use PHPUnit\Framework\TestCase;
 use minga\framework\Context;
+use minga\framework\settings\CacheSettings;
 
 class TestCaseBase extends TestCase
 {
 	public function RemoteProvider()
 	{
 		return [
-			[null],
-			[Context::Settings()->Servers()->Current()->publicUrl],
+			'Sin Remote' => [null],
+			'Con Remote' => [Context::Settings()->Servers()->Current()->publicUrl],
+		];
+	}
+
+	public function CacheSettingProvider()
+	{
+		return [
+			'Sin Cache' => [CacheSettings::Disabled],
+			'Con Cache' => [CacheSettings::Enabled],
 		];
 	}
 
 	public function __construct($name = null, array $data = [], $dataName = '')
 	{
 		Context::Settings()->isTesting = true;
+
+		Context::Settings()->Cache()->Enabled = CacheSettings::Disabled;
+
 		// Context::Settings()->Debug()->debug = false;
 		// Context::Settings()->Debug()->showErrors = false;
 		parent::__construct($name, $data, $dataName);
