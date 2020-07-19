d25f323519caa43ea5ea7bcbba98ea8946c18e76
Rodrigo Queipo <rodrigoq@gmail.com>
2020-07-19 18:24:28 -0300

    Enum (casi) de tipos de footer para emails.

---
+++ src/enums/MailFooter.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace minga\framework\enums;
+
+class MailFooter
+{
+	const None = 0;
+	const News = 1;
+	const Register = 2;
+	const General = 3;
+	const Contact = 4;
+	const LostPassword = 5;
+}
