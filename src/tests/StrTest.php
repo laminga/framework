4b7ce9eb9d6ea3e92a5970dfa94ccb5c999e8942
Rodrigo Queipo <rodrigoq@gmail.com>
2021-06-19 11:59:37 -0300

    Función que chequea emails.

--- src/tests/StrTest.php
+++ src/tests/StrTest.php
@@ -6,7 +6,7 @@ use minga\framework\Str;
 
 class StrTest extends TestCaseBase
 {
-	public function testStartsWithAlfabetic()
+	public function testStartsWithAlfabetic() : void
 	{
 		$this->assertFalse(Str::StartsWithAlfabetic(null));
 		$this->assertFalse(Str::StartsWithAlfabetic(''));
@@ -17,6 +17,27 @@ class StrTest extends TestCaseBase
 		$this->assertTrue(Str::StartsWithAlfabetic('A'));
 		$this->assertTrue(Str::StartsWithAlfabetic('á'));
 		$this->assertTrue(Str::StartsWithAlfabetic('Á'));
+	}
+
+
+	public function testIsEmail() : void
+	{
+		$this->assertTrue(Str::IsEmail('a@b.c'));
+		$this->assertTrue(Str::IsEmail('example@example.com'));
+		$this->assertTrue(Str::IsEmail('example@met.museum'));
+
+		$this->assertFalse(Str::IsEmail(''));
+		$this->assertFalse(Str::IsEmail('a@*.com'));
+		$this->assertFalse(Str::IsEmail('sinarroba'));
+		$this->assertFalse(Str::IsEmail('sin@punto'));
+		$this->assertFalse(Str::IsEmail('sindominio@.com'));
+		$this->assertFalse(Str::IsEmail('@sindireccion.com'));
+		$this->assertFalse(Str::IsEmail('direccionsuperlarga'
+		. 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+		. 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+		. 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+		. 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+		. '@dominio.com'));
 	}
 
 }
