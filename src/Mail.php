7a3f441fd2c3e574deeb62b0ac3abd64b5dcd98c
Pablo De Grande <pablodg@gmail.com>
2021-07-04 15:35:08 -0300

    NoMail como setting

--- src/Mail.php
+++ src/Mail.php
@@ -28,6 +28,9 @@ class Mail
 		if (Context::Settings()->Log()->LogEmailsToDisk)
 			$this->PutToLog();
 
+		if (Context::Settings()->Mail()->NoMail) 
+			return;
+
 		$mail = new PHPMailerSendGrid($throwException);
 
 		$this->SetProvider($mail, $this->to);
