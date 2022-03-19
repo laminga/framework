15469b8b4be88de5bf4c671a12e098cc8e70bd81
Rodrigo Queipo <rodrigoq@gmail.com>
2022-03-19 12:48:07 -0300

    Fix error de tipeo.

--- src/Headers.php
+++ src/Headers.php
@@ -4,23 +4,13 @@ namespace minga\framework;
 
 class Headers
 {
-	//TODO: usar este método!
+	//CORS: Cross-Origin Resource Sharing
 	public static function AcceptAnyCORS() : void
 	{
-		// Resuelve el CORS
 		$origin = Params::SafeServer('HTTP_ORIGIN', '*');
 		header("Access-Control-Allow-Credentials: true");
 		header("Access-Control-Allow-Origin: $origin");
 		header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 		header("Access-Control-Allow-Headers: Origin,X-Requested-With,Content-Type,Accept,Full-Url,Access-Link");
-	}
-
-	//TODO: Borrar esto cuando no tenga llamadores.
-	/**
-	 * @deprecated Error de tipeo, usar AcceptAnyCORS() y borrar este.
-	 */
-	public static function AcceptAnyCOARS() : void
-	{
-		self::AcceptAnyCORS();
 	}
 }
