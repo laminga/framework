afc64e9a74197c1e5499ec5b00e13f44cf836f9c
Rodrigo Queipo <rodrigoq@gmail.com>
2023-08-02 18:46:35 -0300

    Formato.

--- src/Cron.php
+++ src/Cron.php
@@ -18,24 +18,24 @@ class Cron
 		}
 	}
 
-    public static function SaveStatus(string $file, array $job, string $result): void
-    {
-        if (trim($result) == '')
-            $result = 'OK';
-        $job['last_run'] = Date::FormattedDate(time());
+	public static function SaveStatus(string $file, array $job, string $result): void
+	{
+		if (trim($result) == '')
+			$result = 'OK';
+		$job['last_run'] = Date::FormattedDate(time());
 		$job['result'] = $result;
 		IO::WriteJson($file, $job, true);
-    }
+	}
 
 	private static function ShouldRun(array $job) : bool
 	{
 		if($job['enabled'] == false)
 			return false;
 
-        if (isset($job['last_run']))
-            $lastRun = Date::FormattedDateToDateTime($job['last_run']);
-        else
-            return true;
+		if (isset($job['last_run']) == false)
+			return true;
+
+		$lastRun = Date::FormattedDateToDateTime($job['last_run']);
 
 		$timeNextRun = $lastRun->add(self::GetInterval($job));
 		return $timeNextRun <= new \DateTime();
@@ -54,17 +54,17 @@ class Cron
 	}
 
 	private function CallScript(array $job): string
-    {
-        $cli = Context::Settings()->Servers()->PhpCli;
-        $linesOut = [];
-        $ret = System::Execute(
-            $cli,
-            [Context::Paths()->GetCronJobsScriptPath() . '/' . $job['script']], $linesOut);
+	{
+		$cli = Context::Settings()->Servers()->PhpCli;
+		$linesOut = [];
+		$ret = System::Execute(
+			$cli,
+			[Context::Paths()->GetCronJobsScriptPath() . '/' . $job['script']], $linesOut);
 		if($ret['return'] != 0)
 			Log::HandleSilentException(new \Exception('Falló CallScript. Job: ' . json_encode($job) . '. Return: ' . json_encode($linesOut)));
-        $result = implode("\n", $linesOut);
+		$result = implode("\n", $linesOut);
 		return $result;
-    }
+	}
 
 	private static function GetInterval(array $job) : \DateInterval
 	{
