d1a3df3b4b3b251371911fb3ea140b7cf8d33c12
Rodrigo Queipo <rodrigoq@gmail.com>
2026-04-12 12:52:21 -0300

    Fix archivo de test para prod.

--- src/tests/WebConnectionTest.php
+++ src/tests/WebConnectionTest.php
@@ -22,7 +22,7 @@ class WebConnectionTest extends TestCaseBase
 
 	private function Clean() : void
 	{
-		IO::Delete(Context::Paths()->GetTempPath() . '/README.md');
+		IO::Delete(Context::Paths()->GetTempPath() . '/phpunit.xml');
 		IO::Delete(Context::Paths()->GetTempPath() . '/cookie.txt');
 		IO::Delete(Context::Paths()->GetTempPath() . '/log.txt');
 		IO::Delete(Context::Paths()->GetTempPath() . '/log.txt.extra.txt');
@@ -32,15 +32,15 @@ class WebConnectionTest extends TestCaseBase
 	public function testGet() : void
 	{
 		$base = Context::Paths()->GetTempPath();
-		$url = "https://raw.githubusercontent.com/laminga/framework/refs/heads/master/README.md";
-		$size = filesize(realpath(Context::Paths()->GetFrameworkPath() . '/../README.md'));
+		$url = "https://raw.githubusercontent.com/laminga/framework/refs/heads/master/phpunit.xml";
+		$size = filesize(realpath(Context::Paths()->GetFrameworkPath() . '/../phpunit.xml'));
 
 		$wc = new WebConnection();
 		$wc->Initialize($base);
-		$wc->Get($url, $base . "/README.md");
+		$wc->Get($url, $base . "/phpunit.xml");
 		$wc->Finalize();
-		$this->assertTrue(file_exists($base . '/README.md'));
-		$this->assertEquals(filesize($base . '/README.md'), $size);
+		$this->assertTrue(file_exists($base . '/phpunit.xml'));
+		$this->assertEquals(filesize($base . '/phpunit.xml'), $size);
 		$this->assertTrue(file_exists($base . '/log.txt'), 'log file');
 		$this->assertTrue(file_exists($base . '/log.txt.extra.txt'), 'log extra file');
 		$this->assertTrue(file_exists($base . '/response.dat'), 'response dat file');
